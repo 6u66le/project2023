aaf50f9f1ebf12ee2b3eb705efef53763460e5e9
6u66le <94377736+6u66le@users.noreply.github.com>
2023-05-30 09:11:36 -0400

    Update playermove.cs

--- playermove.cs
+++ playermove.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class playermove : MonoBehaviour
+public class TopDownMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * moveSpeed;
     }
 }
